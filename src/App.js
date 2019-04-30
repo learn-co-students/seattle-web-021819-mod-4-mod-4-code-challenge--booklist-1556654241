b46d3edc2172fb659876a4424b9e817e7231cf8e
Joshua Loran <joshua.loran@gmail.com>
2019-04-30 14:16:26 -0700

    finished

--- src/App.js
+++ src/App.js
@@ -5,11 +5,65 @@ import BookList from "./containers/BookList";
 import Bookshelf from "./containers/Bookshelf";
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state={
+      bookList: [],
+      bookShelf: [],
+    }
+    this.handleBookAdd = this.handleBookAdd.bind(this)
+    this.handleBookRemove = this.handleBookRemove.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.onChangeHandler = this.onChangeHandler.bind(this)
+  }
+
+  componentDidMount(){
+      fetch('http://localhost:3005/books')
+        .then(res => res.json())
+        .then(json => this.setState({
+          bookList: json,
+        }))
+  }
+
+  handleBookAdd(book){
+    if(this.state.bookShelf.includes(book)){
+      return
+    } 
+    this.setState({
+      bookShelf: [...this.state.bookShelf, book]
+    })
+  }
+
+  handleBookRemove(book){
+    this.setState({
+      bookShelf: this.state.bookShelf.filter(shelfBook => shelfBook !== book)
+    })
+  }
+
+  handleSubmit(book){
+    console.log(book)
+    this.setState({
+      bookList: [...this.state.bookList, book]
+    })
+  }
+
+  onChangeHandler(event){
+    if(event.target.name === 'name'){
+      this.setState({
+        name: event.target.value
+      })
+    } else {
+      this.setState({
+        img: event.target.value
+      })
+    }
+  }
+
   render() {
     return (
       <div className="book-container">
-        <BookList />
-        <Bookshelf />
+        <BookList name={this.state.name} img={this.state.img} onChangeHandler={this.onChangeHandler} handleSubmit={this.handleSubmit} handleBookAdd={this.handleBookAdd} bookList={this.state.bookList}/>
+        <Bookshelf handleBookRemove={this.handleBookRemove} bookShelf={this.state.bookShelf}/>
       </div>
     );
   }
