6f5643293747c8f5bc8175f5d657beb9d1bcc7a8
github username <github email address>
2019-04-30 14:33:42 -0700

    completed as much as i could

--- src/App.js
+++ src/App.js
@@ -4,12 +4,65 @@ import "./App.css";
 import BookList from "./containers/BookList";
 import Bookshelf from "./containers/Bookshelf";
 
+const BOOK_URL = 'http://localhost:3005/books'
+
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      booklist: [],
+      bookshelf: []
+    }
+    this.addBookToShelf = this.addBookToShelf.bind(this)
+    this.removeFromShelf = this.removeFromShelf.bind(this)
+  }
+
+  handleSubmit(event) {
+    // event.preventDefault();
+    console.log("on app page", event.target.value)
+    console.log("submit button pressed")
+    if (event.target.value === "title") {
+
+    } else if (event.target.value === "author") {
+
+    } else if (event.target.value === "img") {
+
+    }
+  }
+
+  componentDidMount() {
+    fetch(BOOK_URL)
+    .then(resp => resp.json())
+    .then(booklist => {
+      this.setState({ booklist })
+    })
+  }
+
+  addBookToShelf(book) {
+    // if (this.state.bookshelf.includes(book)) return;
+    console.log("on app: book added to bookshelf", book)
+    this.setState({ bookshelf: [...this.state.bookshelf, book]})
+  }
+
+  removeFromShelf(bookToRemove) {
+    const books = this.state.bookshelf.filter(book => {
+      if (book === bookToRemove) {
+        return false;
+      }
+      return true;
+    })
+    this.state({ bookshelf: books })
+  }
+
   render() {
     return (
       <div className="book-container">
-        <BookList />
-        <Bookshelf />
+        <BookList
+          booklist={this.state.booklist}
+          handleSubmit={this.handleSubmit}
+          addBookToShelf={this.addBookToShelf}
+        />
+        <Bookshelf bookshelf={this.state.bookshelf} removeFromShelf={this.removeFromShelf}/>
       </div>
     );
   }
--- src/containers/Bookshelf.js
+++ src/containers/Bookshelf.js
@@ -2,10 +2,13 @@ import React from "react";
 import Book from "../components/Book";
 
 const Bookshelf = props => {
+  const books = props.bookshelf.map(book => {
+    return <Book key={book.id} book={book} handleClick={props.removeFromShelf}/>
+  })
   return (
     <div>
       <h1>Book Shelf</h1>
-      <ul>{/*render list of books here*/}</ul>
+      <ul>{books}</ul>
     </div>
   );
 };
