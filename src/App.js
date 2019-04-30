52dc06ad8a3727b7b83959e29105fdaf7a8658cb
clairemuller <clairermuller@gmail.com>
2019-04-30 14:18:37 -0700

    done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,62 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 import BookList from "./containers/BookList";
 import Bookshelf from "./containers/Bookshelf";
+const URL = 'http://localhost:3005/books';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      books: [],
+      bookshelf: []
+    }
+
+    this.handleBookAddClick = this.handleBookAddClick.bind(this);
+    this.handleBookRemoveClick = this.handleBookRemoveClick.bind(this);
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    fetch(URL)
+    .then(res => res.json())
+    .then(books => this.setState({books}))
+  }
+
+  handleBookAddClick(book) {
+    if (this.state.bookshelf.includes(book)) return
+    this.setState({
+      bookshelf: [...this.state.bookshelf, book]
+    })
+  }
+
+  handleBookRemoveClick(removedBook) {
+    this.setState({
+      bookshelf: this.state.bookshelf.filter(book => book !== removedBook)
+    })
+  }
+
+  handleFormSubmit(newBook) {
+    let newBookList = [...this.state.books, newBook]
+    this.setState({
+      books: newBookList
+    })
+  }
+
   render() {
+    const { books, bookshelf } = this.state
+
     return (
       <div className="book-container">
-        <BookList />
-        <Bookshelf />
+        <BookList
+          books={books}
+          handleBookClick={this.handleBookAddClick}
+          handleFormSubmit={this.handleFormSubmit} />
+        <Bookshelf
+          bookshelf={bookshelf}
+          handleBookClick={this.handleBookRemoveClick} />
       </div>
     );
   }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,62 @@
 import React from "react";
 
 class Form extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      title: '',
+      author: '',
+      img: ''
+    }
+
+    this.handleTitleChange = this.handleTitleChange.bind(this)
+    this.handleAuthorChange = this.handleAuthorChange.bind(this)
+    this.handleImgChange = this.handleImgChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleTitleChange(event) {
+    this.setState({
+      title: event.target.value
+    })
+  }
+
+  handleAuthorChange(event) {
+    this.setState({
+      author: event.target.value
+    })
+  }
+
+  handleImgChange(event) {
+    this.setState({
+      img: event.target.value
+    })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+    this.props.handleFormSubmit(this.state)
+  }
+
   render() {
-    return <h1>{/*create form*/}</h1>;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+        type='text' onChange={this.handleTitleChange}
+        value={this.state.title} placeholder='title' />
+
+        <input
+          type='text' onChange={this.handleAuthorChange}
+          value={this.state.author} placeholder='author' />
+
+        <input
+          type='text' onChange={this.handleImgChange}
+          value={this.state.img} placeholder='img' />
+
+        <input type='submit' />
+      </form>
+    )
   }
 }
 
